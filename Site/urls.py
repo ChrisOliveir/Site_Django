c2f41d00fc0c89e08568594bbd706c6bde66392f
christiane oliveira <christianeolive@hotmail.com>
2022-10-09 18:03:58 -0300

    primeiro

---
+++ Site/urls.py
@@ -0,0 +1,11 @@
+﻿# Não é recomendado criar url no arquivo de rotas do projeto!
+
+from django.urls import path
+
+from .views import index, contato
+
+urlpatterns = [
+    path('',index),
+    path('contato',contato)
+]
+
